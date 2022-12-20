9086c2ba6a5cab47c77fe99cca329afed54481ab
ashwani kumar <arickofficial123@gmail.com>
2022-12-20 15:25:59 +0530

    added nothing

--- lib/screens/health_locker_screen.dart
+++ lib/screens/health_locker_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hk/screens/after_locker_screen.dart';
-import 'package:otp_text_field/otp_text_field.dart';
-import 'package:otp_text_field/style.dart';
 
 class HealthLockerScreen extends StatefulWidget {
   const HealthLockerScreen({super.key});
@@ -11,60 +8,73 @@ class HealthLockerScreen extends StatefulWidget {
 }
 
 class _HealthLockerScreenState extends State<HealthLockerScreen> {
-  String abc='';
+  String abc = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text('Health Locker',style: TextStyle(color: Colors.black),),
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 50,),
-            Container(
-              height: 40,
-              width: 300,
-              child: OTPTextField(
-                fieldWidth: 40,
-                 fieldStyle: FieldStyle.box,
-                outlineBorderRadius: 20,
-                onChanged: (String value){
-                  setState(() {
-                    abc=value;
-                  });
-
-                },
-                // onCompleted: (String value) {
-                //  if(value=='1234'){
-                //    Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                //      return AfterLoker();
-                //    }));
-                //  }
-                //
-                // },
-
-              ),
-            ),
-            SizedBox(height: 50,),
-            Text('enter security pin 1234',style: TextStyle(fontSize: 25,color: Colors.grey.shade300),),
-            SizedBox(height: 100,),
-            ElevatedButton(
-                onPressed: (){
-             if(abc=='1234'){
-                 Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                  return AfterLoker();
-                   }));
-                  }
-                }, child: Text('Enter'))
-          ],
-        ),
-      ),
-    );
+    return Container();
+    // return Scaffold(
+    //   backgroundColor: Colors.white,
+    //   appBar: AppBar(
+    //     backgroundColor: Colors.white,
+    //     centerTitle: true,
+    //     title: const Text(
+    //       'Health Locker',
+    //       style: TextStyle(color: Colors.black),
+    //     ),
+    //     elevation: 0,
+    //   ),
+    //   body: Center(
+    //     child: Column(
+    //       children: [
+    //         const SizedBox(
+    //           height: 50,
+    //         ),
+    //         SizedBox(
+    //           height: 40,
+    //           width: 300,
+    //           child: OTPTextField(
+    //             fieldWidth: 40,
+    //             fieldStyle: FieldStyle.box,
+    //             outlineBorderRadius: 20,
+    //             onChanged: (String value) {
+    //               setState(() {
+    //                 abc = value;
+    //               });
+    //             },
+    //             // onCompleted: (String value) {
+    //             //  if(value=='1234'){
+    //             //    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+    //             //      return AfterLoker();
+    //             //    }));
+    //             //  }
+    //             //
+    //             // },
+    //           ),
+    //         ),
+    //         const SizedBox(
+    //           height: 50,
+    //         ),
+    //         Text(
+    //           'enter security pin 1234',
+    //           style: TextStyle(fontSize: 25, color: Colors.grey.shade300),
+    //         ),
+    //         const SizedBox(
+    //           height: 100,
+    //         ),
+    //         ElevatedButton(
+    //             onPressed: () {
+    //               if (abc == '1234') {
+    //                 Navigator.of(context)
+    //                     .push(MaterialPageRoute(builder: (context) {
+    //                   return const AfterLoker();
+    //                 }));
+    //               }
+    //             },
+    //             child: const Text('Enter'))
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
